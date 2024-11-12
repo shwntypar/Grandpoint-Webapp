b9a982f611d70324785d3c903f1eeebb2d44e587
shwntypar <shawnrada4@gmail.com>
2024-11-12 12:33:21 +0800

    intialize pages

---
+++ grandpoint-web/src/lib/navbar/navbar.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Sidebar from "$lib/sidebar/sidebar.svelte";
+</script>
+
+<div class="ml-[20%] h-20 bg-slate-100 shadow-md">
+    <Sidebar></Sidebar>
+</div>
---
+++ grandpoint-web/src/lib/sidebar/sidebar.svelte
@@ -0,0 +1,22 @@
+<script>
+
+    let tabs = [
+        {name: "Inventory", route:"/" },
+        {name: "Users", route:"/" }
+    ];
+
+</script>
+
+<div class="w-[20%] h-full bg-red-800 absolute left-0 px-5">
+    <div class="flex flex-col justify-center w-full py-4 h-[20%] text-center">
+        <span class=" text-2xl font-bold text-white">GRANDPOINT</span>
+        <span class=" font-bold text-white text-sm">GENERAL MERCHANDISE</span>
+    </div>
+
+    <!-- TABS -->
+    <div class=" mt-6 flex flex-col space-y-3">
+        {#each tabs as tab }
+            <a class="py-2 px-4 rounded-lg text-white font-bold border" href="tab.route">{tab.name}</a>
+        {/each}
+    </div>
+</div>
